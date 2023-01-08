0f5db021155cc4763bdfaf51d4d8de7e413a7965
aeizaguerri <alberto.eizaguerri@gmail.com>
2023-01-08 20:14:11 +0100

    Añadido router a autenticaion

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import users
+from routers import users, jwt_auth_users
 from fastapi.staticfiles import StaticFiles
 
 #Montar server con el comando uvicorn main:app --reload
@@ -10,6 +10,7 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(jwt_auth_users.router)
 app.mount("/static", StaticFiles(directory="static"), name = "static")
 
 @app.get("/") 
